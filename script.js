a0e40f872252cae2a32c5992a04c0ab9d2caceb5
CoderWolfOfT <123480975+CoderWolfOfT@users.noreply.github.com>
2023-01-24 14:15:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 alert("Welcome to Wolf Web")
+var x = document.getElementById("Locate");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Error";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
