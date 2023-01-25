81e8bcb7ae350ec8256fe0eff1626cecf3bd7f9a
CoderWolfOfT <123480975+CoderWolfOfT@users.noreply.github.com>
2023-01-25 10:16:33 -0600

    date

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 alert("Welcome to Wolf Web")
 alert("Tip: For a better experience open a new tab and in search do chrome://quit")
 var x = document.getElementById("Locate");
+const date = document.getElementById("Date")
+const d = new Date("January, 25,2023 10:17")
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
